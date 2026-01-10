edfeeefebb4a840999a44a869a1e4c3bdd26e914
Parikshit Rajpurohit <parikshitrajpurohit56@gmail.com>
2026-01-10 18:41:01 +0530

    Database setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Fuck u nigga</h1>
+      <Button>Yeah baby</Button>
     </div>
   );
 }
